cade950c723f117bf927561410e19c709d94d7fc
AID-Sparkle <kodai.aida@gmail.com>
2025-12-02 14:02:54 +0900

    Add .vscode and .env to .gitignore

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+
+#.envファイル読み込み
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -11,12 +16,15 @@ def index():
 @app.route('/result', methods=['POST'])
 def result():
     data = request.form  # フォームから送られたデータを取得
+    api_key = os.getenv("GOOGLE_MAPS_API_KEY")
+
     return render_template(
         'result.html',
         time=data.get('time'),
         age=data.get('age'),
         gender=data.get('gender'),
-        weather=data.get('weather')
+        weather=data.get('weather'),
+        api_key=api_key
     )
 
 if __name__ == '__main__':
