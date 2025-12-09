1d5a71746fe120206541245a5bec47fb01438f75
AID-Sparkle <kodai.aida@gmail.com>
2025-12-09 15:31:10 +0900

    add Geolocation API and loading.html

--- app.py
+++ app.py
@@ -2,21 +2,30 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
 
-#.envファイル読み込み
 load_dotenv()
 
 app = Flask(__name__)
 
-# 質問入力画面（トップページ）
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# フォーム送信後の結果表示画面
+# 🔹ロード画面
+@app.route('/loading', methods=['GET'])
+def loading():
+    return render_template('loading.html')
+
+# 🔹結果画面
 @app.route('/result', methods=['POST'])
 def result():
-    data = request.form  # フォームから送られたデータを取得
+    data = request.form
     api_key = os.getenv("GOOGLE_MAPS_API_KEY")
+
+    lat = request.form.get("lat")
+    lng = request.form.get("lng")
+
+    lat_clean = lat.replace(".", "") if lat else "取得失敗"
+    lng_clean = lng.replace(".", "") if lng else "取得失敗"
 
     return render_template(
         'result.html',
@@ -24,7 +33,11 @@ def result():
         age=data.get('age'),
         gender=data.get('gender'),
         weather=data.get('weather'),
-        api_key=api_key
+        api_key=api_key,
+        lat=lat,
+        lng=lng,
+        lat_clean=lat_clean,
+        lng_clean=lng_clean
     )
 
 if __name__ == '__main__':
